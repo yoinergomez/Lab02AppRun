2678cd19eb16929c7267e7b8b51f1cccd0b35fc0
yoiner.gomez <yoiner.gomez22@gmail.com>
2016-03-30 07:10:01 -0500

    Creacion de la nueva vista REGISTRO

---
+++ app/src/main/java/co/edu/udea/compumovil/gr02/lab02apprun/actividad/RegisterActivity.java
@@ -0,0 +1,10 @@
+package co.edu.udea.compumovil.gr02.lab02apprun.actividad;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by esteban on 30/03/16.
+ */
+public class RegisterActivity extends AppCompatActivity {
+    
+}
